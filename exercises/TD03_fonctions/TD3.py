246635f565cd1e1fdc2a0d0fc30bbf848baaa5ea
Mathis Pinault <mathis.pinautl@ens.uvsq.fr>
2020-11-25 20:55:40 +0100

    td4

--- exercises/TD03_fonctions/TD3.py
+++ exercises/TD03_fonctions/TD3.py
@@ -1,23 +1,15 @@
-#temps[0] : jours, temps[1]: minutes, temps[2]: minutes, temps[3]: secondes
+def AffichePluriel(val,mot):
+    if val != 0 or val != 1:
+        print(mot, end="")
+    else:
+        print(mot, end="s")
 
-def tempsEnSeconde(temps):
-    """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
-    return (temps[0] * 24) + (temps[1] * 60) + (temps[2] * 60) + temps[3]
+def afficheTemps(temps):
+    temps[0] = jour
+    temps[1] = heure
+    temps[2] = minute
+    temps[3] = seconde
+    return(jour, heure, minute, seconde)
+afficheTemps((1,0,14,23))
 
-temps = (3,23,1,34)
-print(type(temps))
-print(tempsEnSeconde(temps))
-
-def secondeEnTemps(seconde):
-    """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
-    minute = seconde // 60
-    seconde %= 60
-    heure = minute // 60
-    minute %= 60
-    jour = heure // 24
-    heure %= 24
-    return (jour, heure, minute, seconde)
-    
-temps = secondeEnTemps(100000)
-print(temps[0],"jours",temps[1],"heures",temps[2],"minutes",temps[3],"secondes")flake8 
 
